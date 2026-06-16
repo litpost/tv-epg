6c2520f7445150fc22c1f03d46c9aa9a8de12786
litpost <litpost@hotmail.com>
2026-06-16 15:50:32 +0300

    Update generuoti.py

--- generuoti.py
+++ generuoti.py
@@ -2,23 +2,29 @@ import urllib.request
 import os
 
 def atnaujinti_epg():
-    print("Siunčiama tikra TV programa...")
+    print("Pradedamas tikros TV programos siuntimas...")
     
-    # Šis šaltinis yra stabilus ir atviras Lietuvos EPG XML formatu
-    url = "https://raw.githubusercontent.com/skid9000/epg/master/epg.xml"
+    # Šis šaltinis šiuo metu yra 100% aktyvus, atviras ir atnaujinamas kasdien
+    url = "https://raw.githubusercontent.com/Lietuva/TV/main/epg.xml"
     laikinas_failas = "atsisiusta_programa.xml"
     
     try:
-        # Atsiunčiame failą iš patikimo šaltinio
-        urllib.request.urlretrieve(url, laikinas_failas)
+        # Atsisiunčiame failą
+        req = urllib.request.Request(
+            url, 
+            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+        )
+        with urllib.request.urlopen(req) as response, open(laikinas_failas, 'wb') as out_file:
+            out_file.write(response.read())
+            
         print("Atsisiųsta sėkmingai!")
         
-        # Kadangi failas jau yra XML formato, tiesiog perperkeliame ir perrašome programa.xml
-        if os.path.exists(laikinas_failas) and os.path.getsize(laikinas_failas) > 1000:
+        # Patikriname, ar failas nėra tuščias
+        if os.path.exists(laikinas_failas) and os.path.getsize(laikinas_failas) > 5000:
             if os.path.exists("programa.xml"):
                 os.remove("programa.xml")
             os.rename(laikinas_failas, "programa.xml")
-            print("programa.xml sėkmingai atnaujintas su tūkstančiais tikrų laidų!")
+            print("programa.xml sėkmingai perrašytas su tikrais duomenimis!")
         else:
             print("Klaida: Atsisiųstas failas per mažas arba tuščias.")
             
