b6a646f3f82dad738396837a2c428aa22a13591f
litpost <litpost@hotmail.com>
2026-06-16 15:41:14 +0300

    Update generuoti.py

--- generuoti.py
+++ generuoti.py
@@ -1,56 +1,66 @@
-from datetime import datetime, timedelta
+import urllib.request
+import gzip
+import xml.etree.ElementTree as ET
+import os
 
-def generuoti_xml():
-    šiandien = datetime.now()
-    data_str = šiandien.strftime("%Y%m%d")
+def atnaujinti_epg():
+    print("Pradedamas tikros TV programos siuntimas...")
     
-    xml_turinys = f"""<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE tv SYSTEM "xmltv.dtd">
-<tv generator-info-name="Automatinis EPG">
-  <channel id="LRT.lt"><display-name lang="lt">LRT Televizija</display-name></channel>
-  <channel id="TV3.lt"><display-name lang="lt">TV3</display-name></channel>
-  <channel id="LNK.lt"><display-name lang="lt">LNK</display-name></channel>
-  <channel id="BTV.lt"><display-name lang="lt">BTV</display-name></channel>
-
-  <programme start="{data_str}060000 +0300" stop="{data_str}090000 +0300" channel="LRT.lt">
-    <title lang="lt">Labas rytas, Lietuva</title>
-    <desc lang="lt">Informacinė laida, žadinanti Lietuvą kasdien su naujausiomis žiniomis.</desc>
-  </programme>
-  <programme start="{data_str}120000 +0300" stop="{data_str}130000 +0300" channel="LRT.lt">
-    <title lang="lt">LRT Žinios</title>
-    <desc lang="lt">Svarbiausios šalies ir pasaulio naujienos operatyviai.</desc>
-  </programme>
-  <programme start="{data_str}183000 +0300" stop="{data_str}193000 +0300" channel="LRT.lt">
-    <title lang="lt">Dienos tema</title>
-    <desc lang="lt">Aktualijų ir interviu laida su svarbiausiais dienos asmenimis.</desc>
-  </programme>
-
-  <programme start="{data_str}070000 +0300" stop="{data_str}090000 +0300" channel="TV3.lt">
-    <title lang="lt">Nauja diena</title>
-    <desc lang="lt">Rytinė informacinė laida.</desc>
-  </programme>
-  <programme start="{data_str}183000 +0300" stop="{data_str}193000 +0300" channel="TV3.lt">
-    <title lang="lt">TV3 Žinios</title>
-    <desc lang="lt">Karščiausi dienos įvykiai iš Lietuvos ir užsienio.</desc>
-  </programme>
-
-  <programme start="{data_str}183000 +0300" stop="{data_str}193000 +0300" channel="LNK.lt">
-    <title lang="lt">Žinios</title>
-    <desc lang="lt">Išsamiausia dienos naujienų apžvalga.</desc>
-  </programme>
-  <programme start="{data_str}193000 +0300" stop="{data_str}203000 +0300" channel="LNK.lt">
-    <title lang="lt">KK2</title>
-    <desc lang="lt">Aktualijos, satyra ir tyrimai linksmai.</desc>
-  </programme>
-
-  <programme start="{data_str}190000 +0300" stop="{data_str}210000 +0300" channel="BTV.lt">
-    <title lang="lt">Vakaro vaidybinis filmas</title>
-    <desc lang="lt">Pramoginis kinas jūsų namuose.</desc>
-  </programme>
-</tv>"""
+    # Naudojame patikimą, didelį alternatyvų XMLTV šaltinį (pasaulinį / Europos)
+    url = "https://github.com/iptv-org/epg/releases/download/LT_tv24.lt/LT_tv24.lt.xml.gz"
+    gz_failas = "laikinas_epg.xml.gz"
+    xml_failas = "laikinas_epg.xml"
     
-    with open("programa.xml", "w", encoding="utf-8") as f:
-        f.write(xml_turinys)
+    try:
+        # 1. Atsisiunčiame suspaustą programos failą
+        urllib.request.urlretrieve(url, gz_failas)
+        print("Failas sėkmingai atsisiųstas. Išpakuojama...")
+        
+        # 2. Išpakuojame GZ failą
+        with gzip.open(gz_failas, 'rb') as f_in:
+            with open(xml_failas, 'wb') as f_out:
+                f_out.write(f_in.read())
+        
+        print("Failas išpakuotas. Pradedamas filtravimas...")
+        
+        # 3. Nuskaitome XML duomenis
+        tree = ET.parse(xml_failas)
+        root = tree.getroot()
+        
+        # Sukuriame naują, švarų XML dokumentą
+        naujas_root = ET.Element("tv", {
+            "generator-info-name": "Automatinis Lietuvos EPG",
+            "generator-info-url": "https://github.com/litpost/tv-epg"
+        })
+        
+        # Nukopijuojame visus kanalus ir laidas
+        kanalu_skaicius = 0
+        laidu_skaicius = 0
+        
+        for elementas in root:
+            if elementas.tag == "channel" or elementas.tag == "programme":
+                # Šis šaltinis jau yra skirtas Lietuvai, todėl perkeliame viską tiesiogiai
+                naujas_root.append(elementas)
+                if elementas.tag == "channel":
+                    kanalu_skaicius += 1
+                else:
+                    laidu_skaicius += 1
+                    
+        # 4. Išsaugome galutinį programa.xml failą
+        naujas_tree = ET.ElementTree(naujas_root)
+        ET.indent(naujas_tree, space="  ", level=0) # Padarome gražų formatavimą
+        naujas_tree.write("programa.xml", encoding="utf-8", xml_declaration=True)
+        
+        print(f"Sėkmingai sugeneruota! Rasta kanalų: {kanalu_skaicius}, laidų: {laidu_skaicius}")
+        
+    except Exception as e:
+        print(f"Klaida apdorojant duomenis: {e}")
+        # Jei įvyko klaida, paliekame seną failą, kad nesugadintume svetainės
+        
+    finally:
+        # Išvalome laikinus failus, kad neužkimštume talpyklos
+        if os.path.exists(gz_failas): os.remove(gz_failas)
+        if os.path.exists(xml_failas): os.remove(xml_failas)
 
 if __name__ == "__main__":
-    generuoti_xml()
+    atnaujinti_epg()
